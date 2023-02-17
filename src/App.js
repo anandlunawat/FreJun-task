36300748be5eb8636dc17a7f39662aacabd1f1f8
Anand Lunawat <aplunawat@gmail.com>
2023-02-17 19:02:51 +0530

    Ready to build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Login } from './Components/Login';
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Users } from './Components/Users';
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
